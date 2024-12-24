6911366cef47b395a017434b91aed969f90d3071
Johnny Le <johnnyl111@hotmail.com.au>
2024-12-24 11:12:53 +1100

    initial log in and set up password config

--- tests/browsertest.spec.js
+++ tests/browsertest.spec.js
@@ -1,7 +1,16 @@
 import { test, expect } from '@playwright/test'
+import dotenv from 'dotenv';
 
-test('should load home page', async ({ page }) => {
-    await page.goto('https://google.com.au/');
+dotenv.config();
+
+test('should load home page and log in successfully', async ({ page }) => {
+    const password = process.env.PLAYWRIGHT_PASSWORD;
+    await page.goto('https://app.todoist.com//');
     const title = await page.title()
-    expect(title).toBe('Google');
+    expect(title).toBe('Log in to Todoist');
+
+    await page.fill('input[type="email"]', 'johnnyl111@hotmail.com.au');
+    await page.fill('input[type="password"]', password);
+
+    await page.click('button[type="submit"]');
 });
