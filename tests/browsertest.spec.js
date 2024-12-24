7997b79f5a82ab8b0fbbd7fa428d14382883d119
Johnny Le <johnnyl111@hotmail.com.au>
2024-12-24 11:21:40 +1100

    add initial test & log in

--- tests/browsertest.spec.js
+++ tests/browsertest.spec.js
@@ -1,7 +1,12 @@
 import { test, expect } from '@playwright/test'
 
-test('should load home page', async ({ page }) => {
-    await page.goto('https://google.com.au/');
-    const title = await page.title()
-    expect(title).toBe('Google');
+test('should load home page and log in', async ({ page }) => {
+    await page.goto('https://app.todoist.com/');
+    const title = await page.title();
+    expect(title).toBe('Log in to Todoist');
+
+    await page.fill('input[type="email"]', 'johnnyl111@hotmail.com.au');
+    await page.fill('input[type="password"]', 'easygo123');
+
+    await page.click('button[type="submit"]');
 });
